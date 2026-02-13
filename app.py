470f9a7799e08b1b6da43ad986df95a3189a5da2
anja-crtl <xboxharibo@gmx.de>
2026-02-13 23:08:43 +0100

    Update Flask app to handle form submissions
    Refactor home route to handle POST requests and render index.html.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from scripts import script
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "läuft!"
+    results = None
+    if request.method == 'POST':
+        results = script.main()
+    return render_template("index.html", results=results)
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
